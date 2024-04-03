33dc55eeff7e54fa78a4cfb711ea309abc420c53
pratiksha <bhoitepratiksha05@gmail.com>
2024-04-03 10:31:41 +0530

    callbacks

---
+++ Understanding Callbacks - Question 1/index.js
@@ -0,0 +1,25 @@
+function buyBike(callback)
+{
+  setTimeout(function() {
+    console.log('Bought Royal Enfield Himalayan');
+     callback();
+  },2000);
+}
+function planTrip()
+{
+  setTimeout(function() {
+    console.log('Trip to Ladakh planned');
+   
+  },1000);
+}
+buyBike(planTrip);
+
+
+
+
+
+// Do not touch the code below:
+module.exports = {
+  buyBike,
+  planTrip,
+};
