f2dfa7b1e1a253707a28a14313fd7342afd360e2
pratiksha <bhoitepratiksha05@gmail.com>
2024-04-04 09:25:14 +0530

    callbacks vs promises

---
+++ Callbacks Versus Promises - Question 1/index.js
@@ -0,0 +1,53 @@
+function buyBike()
+{
+  return new Promise(function(resolve,reject){
+  setTimeout(function() {
+    resolve("Bought Royal Enfield Himalayan");
+  },2000);
+});
+}
+
+function planTrip()
+{
+  return new Promise(function(resolve,reject) {
+    setTimeout(function() {
+      resolve('Trip to Ladakh planned');
+    },1000);
+  });
+}
+
+function reachLadakh()
+{
+  return new Promise(function(resolve,reject) {
+    setTimeout(function() {
+      resolve('Reached Ladakh');
+    },1000);
+  });
+}
+
+function visitPangongLake()
+{
+  return new Promise(function(resolve,reject) {
+    setTimeout(function() {
+      resolve('Visited Pangong Lake');
+    },500);
+  });
+}
+
+buyBike().then(function (result){
+  console.log(result);
+  planTrip().then(function (result){
+  console.log(result);
+    reachLadakh().then(function (result){
+    console.log(result);
+      visitPangongLake();
+});
+});
+});
+// Do not touch the code below:
+module.exports = {
+  buyBike,
+  planTrip,
+  reachLadakh,
+  visitPangongLake,
+};
